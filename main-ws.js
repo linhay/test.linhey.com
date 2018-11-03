baec635eb17d913cf1d7ff586d4c5ad67f416e2b
linhay <158179948@qq.com>
2018-11-03 17:11:12 +0800

    update

--- main-ws.js
+++ main-ws.js
@@ -7,27 +7,5 @@ wss.on('connection', function connection(ws, req) {
         console.log('received: %s', message);
     });
     ws.send('headers: ' + JSON.stringify(req.headers));
-    ws.send('ip: ' + JSON.stringify(getIP));
+    ws.send('ip: ' + JSON.stringify(ws._socket.address()));
 });
-
-
-
-
-function getIP() {
-    var os = require('os');
-    var ifaces = os.networkInterfaces();
-    ips = []
-    Object.keys(ifaces).forEach(function (ifname) {
-        var alias = 0;
-        ifaces[ifname].forEach(function (iface) {
-            if ('IPv4' !== iface.family || iface.internal !== false) { return; }
-            if (alias >= 1) {
-                ips.push(ifname + alias + ':' + iface.address);
-            } else {
-                ips.push(ifname + ':' + iface.address);
-            }
-            ++alias;
-        });
-    });
-    return ips
-}
